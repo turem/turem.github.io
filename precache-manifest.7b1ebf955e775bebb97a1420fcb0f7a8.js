0dd05f9e5bd5c95cba497d624c756b5c800d0ad8
marcin.turemka <marcin.turemka@sollers.eu>
2020-08-18 08:51:23 +0200

    Updates

---
+++ precache-manifest.7b1ebf955e775bebb97a1420fcb0f7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0da842460f33f1926c1b",
+    "url": "/static/css/main.a2915a6d.chunk.css"
+  },
+  {
+    "revision": "0da842460f33f1926c1b",
+    "url": "/static/js/main.3bc99ed5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86c7a659b8326c7a8568",
+    "url": "/static/js/2.a6827223.chunk.js"
+  },
+  {
+    "revision": "1831f81eb7a05a09773adf7ba5385a99",
+    "url": "/static/media/background.1831f81e.jpg"
+  },
+  {
+    "revision": "ee7562b7c4f100900349a17cd3ea7567",
+    "url": "/index.html"
+  }
+];
